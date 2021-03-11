eb1cc3144e2ae0176bda4f5fa2ebcf98c218dce1
gabriele <sozziga@gmail.com>
2021-03-11 16:54:00 +0100

    modifiche

--- main.js
+++ main.js
@@ -2,14 +2,19 @@
 $(document).ready(function(){
   var prev = $(".prev .fa-angle-left")
   var next = $(".next .fa-angle-right")
-  var imgActive = $(".images img.active")
+  var active = $(".active")
 
 
-   next.click(function() {
-    imgActive.removeClass("active");
-    imgActive.next().addClass("active");
+
+  next.click(function() {
+    active.removeClass("active");
+    active.next().addClass("active");
 
   })
 
+  prev.click(function(){
+    active.removeClass("active");
+    active.prev().addClass("active");
+  })
 
 })
