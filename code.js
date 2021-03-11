087e317e91fb04665137d54aa0fabe1bd7fd7af1
gabriele <sozziga@gmail.com>
2021-03-11 17:44:20 +0100

    Completato
    Ho inserito un ulteriore file js (e il relativo script nell'html) in cui ho scritto il codice e poi nel main ho richiamato le funzioni. Ho voluto fare così per esercitarmi con quanto fatto vedere a lezione da Raffaele.

---
+++ code.js
@@ -0,0 +1,29 @@
+var prev = $(".prev .fa-angle-left");
+var next = $(".next .fa-angle-right");
+var last = $(".last")
+var first = $(".first")
+
+
+function nextImg() {
+
+  next.click(function() {
+    var active = $(".active");
+    active.removeClass("active");
+    active.next().addClass("active");
+    if (active.hasClass("last")) {
+      first.addClass("active")
+    }
+  })
+}
+
+function prevImg() {
+
+  prev.click(function(){
+    var active = $(".active");
+    active.removeClass("active");
+    active.prev().addClass("active");
+    if (active.hasClass("first")) {
+      last.addClass("active")
+    }
+  })
+}
--- main.js
+++ main.js
@@ -1,20 +1,6 @@
 
 $(document).ready(function(){
-  var prev = $(".prev .fa-angle-left")
-  var next = $(".next .fa-angle-right")
-  var active = $(".active")
 
-
-
-  next.click(function() {
-    active.removeClass("active");
-    active.next().addClass("active");
-
-  })
-
-  prev.click(function(){
-    active.removeClass("active");
-    active.prev().addClass("active");
-  })
-
+  nextImg();
+  prevImg();
 })
